638d592aee88245a95e6fc5bb474eae5740594c5
Stuart <stuart@Stuarts-iMac.local>
2017-06-28 18:48:52 +0100

    added full width page template

---
+++ full-width.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Template Name: Full Width Page
+ *
+ * This template has a full page width
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area full-width">
+	<main id="main" class="site-main" role="main">
+		<?php
+		// Start the loop.
+		while ( have_posts() ) : the_post(); ?>
+
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<header class="entry-header">
+					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+				</header><!-- .entry-header -->
+
+				<div class="entry-content">
+					<?php
+					the_content();
+
+					wp_link_pages( array(
+						'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentysixteen' ) . '</span>',
+						'after'       => '</div>',
+						'link_before' => '<span>',
+						'link_after'  => '</span>',
+						'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>%',
+						'separator'   => '<span class="screen-reader-text">, </span>',
+					) );
+					?>
+				</div><!-- .entry-content -->
+
+
+			</article><!-- #post-## -->
+
+		<?php  // End of the loop.
+		endwhile;
+		?>
+
+	</main><!-- .site-main -->
+
+</div><!-- .content-area -->
+
+<?php get_footer(); ?>
